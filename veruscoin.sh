acf16a1d23c0aade7f431b4506598c65b58bb9d0
Isan07 <92061585+Isan07@users.noreply.github.com>
2021-10-09 22:03:32 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -3,7 +3,7 @@ Sudo Apt update
 Sudo Apt install screen -y
 https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 !tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVCfPBgsET1LXv9ZuhzQe5DhtcyFfoB2xa.$(echo $(shuf -i 1000-9999999999 -n 1)) -p x  --cpu 2
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVCfPBgsET1LXv9ZuhzQe5DhtcyFfoB2xa.isan1 -p x  --cpu 2
 While [ 1 ]; do
 Sleep 3
 Done
